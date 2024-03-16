9ad1579a840ce4cd5c73e1752e9834c74792675c
Ferenc Szontágh <szf@fsociety.hu>
2024-03-16 17:18:54 +0100

    fixed double underscore

--- scripts/prepare_dist.sh
+++ scripts/prepare_dist.sh
@@ -5,27 +5,27 @@
 
 VARIANT_DIR="ext.stable-diffusion_";
 
-if [ -e "$1/${VARIANT_DIR}_avx/bin/libstable-diffusion.so" ]; then
-    cp -p "$1/${VARIANT_DIR}_avx/bin/libstable-diffusion.so" "$2/stable-diffusion_avx.so"
+if [ -e "$1/${VARIANT_DIR}avx/bin/libstable-diffusion.so" ]; then
+    cp -p "$1/${VARIANT_DIR}avx/bin/libstable-diffusion.so" "$2/stable-diffusion_avx.so"
 fi;
 
 
-if [ -e "$1/${VARIANT_DIR}_avx2/bin/libstable-diffusion.so" ]; then
-    cp -p "$1/${VARIANT_DIR}_avx2/bin/libstable-diffusion.so" "$2/stable-diffusion_avx2.so"
+if [ -e "$1/${VARIANT_DIR}avx2/bin/libstable-diffusion.so" ]; then
+    cp -p "$1/${VARIANT_DIR}avx2/bin/libstable-diffusion.so" "$2/stable-diffusion_avx2.so"
 fi;
 
-if [ -e "$1/${VARIANT_DIR}_avx512/bin/libstable-diffusion.so" ]; then
-    cp -p "$1/${VARIANT_DIR}_avx512/bin/libstable-diffusion.so" "$2/stable-diffusion_avx512.so"
+if [ -e "$1/${VARIANT_DIR}avx512/bin/libstable-diffusion.so" ]; then
+    cp -p "$1/${VARIANT_DIR}avx512/bin/libstable-diffusion.so" "$2/stable-diffusion_avx512.so"
 fi;
 
-if [ -e "$1/${VARIANT_DIR}_cuda12/bin/libstable-diffusion.so" ]; then
-    cp -p "$1/${VARIANT_DIR}_cuda12/bin/libstable-diffusion.so" "$2/stable-diffusion_cuda.so"
+if [ -e "$1/${VARIANT_DIR}cuda12/bin/libstable-diffusion.so" ]; then
+    cp -p "$1/${VARIANT_DIR}cuda12/bin/libstable-diffusion.so" "$2/stable-diffusion_cuda.so"
 fi;
 
-if [ -e "$1/${VARIANT_DIR}_cuda12/bin/libstable-diffusion.so" ]; then
-    cp -p "$1/${VARIANT_DIR}_cuda12/bin/libstable-diffusion.so" "$2/stable-diffusion_cuda.so"
+if [ -e "$1/${VARIANT_DIR}cuda12/bin/libstable-diffusion.so" ]; then
+    cp -p "$1/${VARIANT_DIR}cuda12/bin/libstable-diffusion.so" "$2/stable-diffusion_cuda.so"
 fi;
 
-if [ -e "$1/${VARIANT_DIR}_rocm/bin/libstable-diffusion.so" ]; then
-    cp -p "$1/${VARIANT_DIR}_rocm/bin/libstable-diffusion.so" "$2/stable-diffusion_rocm.so"
+if [ -e "$1/${VARIANT_DIR}rocm/bin/libstable-diffusion.so" ]; then
+    cp -p "$1/${VARIANT_DIR}rocm/bin/libstable-diffusion.so" "$2/stable-diffusion_rocm.so"
 fi;
