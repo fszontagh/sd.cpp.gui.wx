cd878d00c7ed2b207f22e64c6d18ff116a103a2a
Ferenc Szontágh <szf@fsociety.hu>
2024-03-16 17:40:14 +0100

    fixed path

--- scripts/prepare_dist.sh
+++ scripts/prepare_dist.sh
@@ -6,26 +6,26 @@
 VARIANT_DIR="ext.stable-diffusion.cpp_";
 
 if [ -e "$1/${VARIANT_DIR}avx/bin/libstable-diffusion.so" ]; then
-    cp -p "$1/${VARIANT_DIR}avx/bin/libstable-diffusion.so" "$2/stable-diffusion_avx.so"
+    cp -p "$1/${VARIANT_DIR}avx/bin/libstable-diffusion.so" "$2/all/stable-diffusion_avx.so"
 fi;
 
 
 if [ -e "$1/${VARIANT_DIR}avx2/bin/libstable-diffusion.so" ]; then
-    cp -p "$1/${VARIANT_DIR}avx2/bin/libstable-diffusion.so" "$2/stable-diffusion_avx2.so"
+    cp -p "$1/${VARIANT_DIR}avx2/bin/libstable-diffusion.so" "$2/all/stable-diffusion_avx2.so"
 fi;
 
 if [ -e "$1/${VARIANT_DIR}avx512/bin/libstable-diffusion.so" ]; then
-    cp -p "$1/${VARIANT_DIR}avx512/bin/libstable-diffusion.so" "$2/stable-diffusion_avx512.so"
+    cp -p "$1/${VARIANT_DIR}avx512/bin/libstable-diffusion.so" "$2/all/stable-diffusion_avx512.so"
 fi;
 
 if [ -e "$1/${VARIANT_DIR}cuda12/bin/libstable-diffusion.so" ]; then
-    cp -p "$1/${VARIANT_DIR}cuda12/bin/libstable-diffusion.so" "$2/stable-diffusion_cuda.so"
+    cp -p "$1/${VARIANT_DIR}cuda12/bin/libstable-diffusion.so" "$2/all/stable-diffusion_cuda.so"
 fi;
 
 if [ -e "$1/${VARIANT_DIR}cuda12/bin/libstable-diffusion.so" ]; then
-    cp -p "$1/${VARIANT_DIR}cuda12/bin/libstable-diffusion.so" "$2/stable-diffusion_cuda.so"
+    cp -p "$1/${VARIANT_DIR}cuda12/bin/libstable-diffusion.so" "$2/all/stable-diffusion_cuda.so"
 fi;
 
 if [ -e "$1/${VARIANT_DIR}rocm/bin/libstable-diffusion.so" ]; then
-    cp -p "$1/${VARIANT_DIR}rocm/bin/libstable-diffusion.so" "$2/stable-diffusion_rocm.so"
+    cp -p "$1/${VARIANT_DIR}rocm/bin/libstable-diffusion.so" "$2/all/stable-diffusion_rocm.so"
 fi;
