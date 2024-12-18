03c1c0f483da1402558d513364aea3da712a7022
fszontagh <szf@fsociety.hu>
2024-12-18 20:20:29 +0100

    search for cpu_features on system if installed, init remote diffusion socket server

---
+++ server/src/pch.h
@@ -0,0 +1,31 @@
+#include <chrono>
+#include <iostream>
+#include <memory>
+#include <ostream>
+#include <string>
+#include <thread>
+#include <random>
+
+#if defined(_WIN32) || defined(_WIN64)
+#include <windows.h>
+#else
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <cstring>
+#endif
+
+#include "libs/json.hpp"
+
+#include "ver.hpp"
+#include "config.hpp"
+
+#include <wx/event.h>
+#include <wx/string.h>
+#include <wx/translation.h>
+#include <wx/filename.h>
+#include "helpers/sd.hpp"
+#include "helpers/sslUtils.hpp"
+#include "libs/SharedLibrary.h"
+#include "libs/SharedMemoryManager.h"
+#include "ui/QueueManager.h"
