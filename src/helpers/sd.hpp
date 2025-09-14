be255dcd913a302dea8de79107cc221ba1ee7734
Fszontagh <fszontagh@example.com>
2025-09-14 21:01:55 +0200

    Update stable diffusion API to match latest changes

--- src/helpers/sd.hpp
+++ src/helpers/sd.hpp
@@ -28,8 +28,10 @@ typedef struct {
     const char* model_path;
     const char* clip_l_path;
     const char* clip_g_path;
+    const char* clip_vision_path;
     const char* t5xxl_path;
     const char* diffusion_model_path;
+    const char* high_noise_diffusion_model_path;
     const char* vae_path;
     const char* taesd_path;
     const char* control_net_path;
@@ -42,14 +44,17 @@ typedef struct {
     int n_threads;
     enum sd_type_t wtype;
     enum rng_type_t rng_type;
-    enum schedule_t schedule;
+    bool offload_params_to_cpu;
     bool keep_clip_on_cpu;
     bool keep_control_net_on_cpu;
     bool keep_vae_on_cpu;
     bool diffusion_flash_attn;
+    bool diffusion_conv_direct;
+    bool vae_conv_direct;
     bool chroma_use_dit_mask;
     bool chroma_use_t5_mask;
     int chroma_t5_mask_pad;
+    float flow_shift;
 } sd_ctx_params_t;
 
 typedef struct {
@@ -63,29 +68,34 @@ typedef struct {
 typedef struct {
     float txt_cfg;
     float img_cfg;
-    float min_cfg;
     float distilled_guidance;
     sd_slg_params_t slg;
 } sd_guidance_params_t;
 
 typedef struct {
-    const char* prompt;
-    const char* negative_prompt;
-    int clip_skip;
     sd_guidance_params_t guidance;
-    sd_image_t init_image;
-    sd_image_t* ref_images;
-    int ref_images_count;
-    sd_image_t mask_image;
-    int width;
-    int height;
+    enum schedule_t scheduler;
     enum sample_method_t sample_method;
     int sample_steps;
     float eta;
+} sd_sample_params_t;
+
+typedef struct {
+    const char* prompt;
+    const char* negative_prompt;
+    int clip_skip;
+    sd_image_t init_image;
+    sd_image_t* ref_images;
+    int ref_images_count;
+    bool increase_ref_index;
+    sd_image_t mask_image;
+    int width;
+    int height;
+    sd_sample_params_t sample_params;
     float strength;
     int64_t seed;
     int batch_count;
-    const sd_image_t* control_cond;
+    sd_image_t control_image;
     float control_strength;
     float style_strength;
     bool normalize_input;
@@ -93,18 +103,19 @@ typedef struct {
 } sd_img_gen_params_t;
 
 typedef struct {
+    const char* prompt;
+    const char* negative_prompt;
+    int clip_skip;
     sd_image_t init_image;
+    sd_image_t end_image;
     int width;
     int height;
-    sd_guidance_params_t guidance;
-    enum sample_method_t sample_method;
-    int sample_steps;
+    sd_sample_params_t sample_params;
+    sd_sample_params_t high_noise_sample_params;
+    float moe_boundary;
     float strength;
     int64_t seed;
     int video_frames;
-    int motion_bucket_id;
-    int fps;
-    float augmentation_level;
 } sd_vid_gen_params_t;
 
 typedef struct sd_ctx_t sd_ctx_t;
@@ -121,14 +132,17 @@ typedef char* (*SdCtxParamsToStrFunction)(const sd_ctx_params_t*);
 typedef sd_ctx_t* (*NewSdCtxFunction)(const sd_ctx_params_t*);
 typedef void (*FreeSdCtxFunction)(sd_ctx_t*);
 
+typedef void (*SdSampleParamsInitFunction)(sd_sample_params_t*);
+typedef char* (*SdSampleParamsToStrFunction)(const sd_sample_params_t*);
+
 typedef void (*SdImgGenParamsInitFunction)(sd_img_gen_params_t*);
 typedef char* (*SdImgGenParamsToStrFunction)(const sd_img_gen_params_t*);
 typedef sd_image_t* (*GenerateImageFunction)(sd_ctx_t*, const sd_img_gen_params_t*);
 
 typedef void (*SdVidGenParamsInitFunction)(sd_vid_gen_params_t*);
-typedef sd_image_t* (*GenerateVideoFunction)(sd_ctx_t*, const sd_vid_gen_params_t*);
+typedef sd_image_t* (*GenerateVideoFunction)(sd_ctx_t*, const sd_vid_gen_params_t*, int*);
 
-typedef upscaler_ctx_t* (*NewUpscalerCtxFunction)(const char*, int);
+typedef upscaler_ctx_t* (*NewUpscalerCtxFunction)(const char*, bool, bool, int);
 typedef void (*FreeUpscalerCtxFunction)(upscaler_ctx_t*);
 typedef sd_image_t (*UpscalerFunction)(upscaler_ctx_t*, sd_image_t, uint32_t);
 
